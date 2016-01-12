cf18d386ac3136d3e227957d5c16293be183fc45
andziaoo7 <a.kalamacka@gmail.com>
2016-01-12 23:24:47 +0100

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 // grab our gulp packages
 var gulp = require('gulp');
-// var gutil = require('gulp-util');
+var gutil = require('gulp-util');
+var connect = require('gulp-connect');
+var server = require('gulp-server-livereload');
+
 
 // create a default task and just log a message
-// gulp.task('default', function() {
-//   return gutil.log('Gulp is running!')
-// });
+gulp.task('default', function() {
+  return gutil.log('Gulp is running!')
+});
 
-var connect = require('gulp-connect');
 
+// run webserver
 gulp.task('connect', function() {
   connect.server({
-    root: 'portfolio/',
-    port: 8888
+    root: '.',
+    livereload: true,
+    port: 8000,
+    open: true
   });
 });
+
+// watch for any changes in files
+  gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['js']);
+    gulp.watch('css/main.css', ['css']);
+  });
+
+
+// });
